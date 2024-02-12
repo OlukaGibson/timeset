55e46ec6d5f4f79a3d95b66cb00d96d05216b371
OlukaGibson <gibsonoluka7@gmail.com>
2024-02-12 10:12:44 +0300

    work_out

--- timeapp/views.py
+++ timeapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 def time(request):
-    return "Hello, Gibson!"
+    return HttpResponse('Hello, Gibson!')
