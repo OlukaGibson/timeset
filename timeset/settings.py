411459c7df09ed7cc0a02df31d561c1654761e59
OlukaGibson <gibsonoluka7@gmail.com>
2024-02-12 09:41:07 +0300

    work_out

--- timeset/settings.py
+++ timeset/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timeset.apps.TimesetConfig',
+    'timeapp.apps.TimeappConfig',
 ]
 
 MIDDLEWARE = [
